e5afdbb2fa92b50f1898f5915d387586de811e38
wr_glb <goodMorning_glb@qq.com>
2020-12-15 16:29:18 +0800

    wr community

---
+++ src/main/java/life/majiang/community/dto/GithubUser.java
@@ -0,0 +1,35 @@
+package life.majiang.community.dto;
+
+/**
+ * @author WR
+ * @create 2020-12-15 15:49
+ */
+public class GithubUser {
+    private String name;
+    private long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
